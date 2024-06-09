b0bec8b6ee129f3874774d0bb995698ce4ce56bb
yago-25 <yagofontanez2000@gmail.com>
2024-06-08 23:53:23 -0300

    fix: arrumando files de rotas e configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Loading from './pages/Loading';
 import Principal from './pages/Principal';
 import './index.css';
 
 const App: React.FC = () => {
-  const location = useLocation();
-
   return (
-    <Routes location={location}>
+    <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/loading" element={<Loading />} />
       <Route path="/home" element={<Principal />} />
